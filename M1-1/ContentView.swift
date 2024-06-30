31b1e869849a028dcd0f6909e50a8ad9be588ebb
kangy106 <kangy106@gmail.com>
2024-06-30 20:14:03 +0900

    Initial Commit

---
+++ M1-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M1-1
+//
+//  Created by Nagayama Kazuki on 2024/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M1-1/M1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  M1_1App.swift
+//  M1-1
+//
+//  Created by Nagayama Kazuki on 2024/06/30.
+//
+
+import SwiftUI
+
+@main
+struct M1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
